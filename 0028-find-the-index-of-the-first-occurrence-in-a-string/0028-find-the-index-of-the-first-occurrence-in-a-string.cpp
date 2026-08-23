bdd51b182c1d073bea49f87bcac6b5abeb06bfbc
Aryan <officialaryan1194@gmail.com>
2026-08-23 15:49:36 +0530

    Time: 0 ms (100%), Space: 9.4 MB (10.35%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,26 +1,62 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int n = haystack.size();
-        int m = needle.size();
 
-        for(int i=0;i<=n-m;i++){
-            int first=i;
-            int second=0;
+    void lpsfind(vector<int>& lps, string s) {
+        int pre = 0, suf = 1;
 
-            while(second<m){
-                if(haystack[first]!=needle[second]){
-                    break;
-                }else{
-                    first++,second++;
+        while(suf < s.size()) {
+            if(s[pre] == s[suf]) {
+                lps[suf] = pre + 1;
+                pre++;
+                suf++;
+            }
+            else {
+                if(pre == 0) {
+                    lps[suf] = 0;
+                    suf++;
+                }
+                else {
+                    pre = lps[pre - 1];
                 }
             }
+        }
+    }
 
-            if(second==m){
-                return first-second;
+    int strStr(string haystack, string needle) {
+
+        int n = needle.size();
+        int m = haystack.size();
+
+        vector<int> lps(n, 0);
+
+        // Create LPS array
+        lpsfind(lps, needle);
+
+        int first = 0;
+        int second = 0;
+
+        // KMP search
+        while(first < m && second < n) {
+
+            if(haystack[first] == needle[second]) {
+                first++;
+                second++;
+            }
+            else {
+                if(second == 0) {
+                    first++;
+                }
+                else {
+                    second = lps[second - 1];
+                }
             }
         }
 
-        return -1;
+        if(second == n) {
+            return first - second;
+        }
+        else {
+            return -1;
+        }
     }
 };
